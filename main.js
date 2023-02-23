327294664873b45627a076fc5f531d66daddbc3e
MatPre <mapresta00@gmail.com>
2023-02-23 11:00:06 -0300

    Corrección de errores

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 //Ingresa la frase y la encripta
 function encriptar() {
-
-    var text = document.getElementById("texto").value;
-    text = text.replace(/e/gi, "enter");
-    text = text.replace(/i/gi, "imes");
-    text = text.replace(/a/gi, "ai");
-    text = text.replace(/o/gi, "ober");
-    text = text.replace(/u/gi, "ufat");
-    document.getElementById("textout").value = text;
-    document.getElementById("text").value = "";                
+  var text = document.getElementById("texto").value;
+  text = text.replace(/e/gi, "enter");
+  text = text.replace(/i/gi, "imes");
+  text = text.replace(/a/gi, "ai");
+  text = text.replace(/o/gi, "ober");
+  text = text.replace(/u/gi, "ufat");
+  document.getElementById("textout").value = text;
+  document.getElementById("text").value = "";
 }
 
 //ingresa la frase y la desencripta
 function desencriptar() {
-
-    var text = document.getElementById("texto").value;
-    text = text.replace(/enter/gi, "e");
-    text = text.replace(/imes/gi, "i");
-    text = text.replace(/ai/gi, "a");
-    text = text.replace(/ober/gi, "o");
-    text = text.replace(/ufat/gi, "u");
-    document.getElementById("textout").value = text;
-    document.getElementById("text").value = "";
-    
+  var text = document.getElementById("texto").value;
+  text = text.replace(/enter/gi, "e");
+  text = text.replace(/imes/gi, "i");
+  text = text.replace(/ai/gi, "a");
+  text = text.replace(/ober/gi, "o");
+  text = text.replace(/ufat/gi, "u");
+  document.getElementById("textout").value = text;
+  document.getElementById("text").value = "";
 }
 
 //Copia el contenido del cuadro de salida
 function copiar() {
-
-    navigator.clipboard.writeText(document.getElementById("textout").value);  
-    document.getElementById("textout").value = "";
+  navigator.clipboard.writeText(document.getElementById("textout").value);
+  document.getElementById("textout").value = "";
 }
